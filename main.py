be5d851073aa94479440b2ab1cdf849af8a69816
timcliffe <timothy.h.cliffe@gmail.com>
2017-10-08 16:20:26 -0500

    stuff

---
+++ main.py
@@ -0,0 +1,59 @@
+from flask import Flask, request, redirect, request, render_template
+from flask_sqlalchemy import flask_sqlalchemy
+
+app = Flask(__name__)
+ap.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql.pymysql://build-a-blog:Angels1234@localhost:8889/build-a-blog"
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+
+class Blog(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120))
+    body = db.Column(db.String(1000))
+
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
+
+
+
+
+
+@app.route('/newpost', methods=['POST'])
+def add_blog():
+    if request.method == 'GET':
+        return render_template('newpost.html', title="Add Blog Entry"
+
+    if request.method = 'POST':
+        blog_title = request.form['title']
+        blog_body = request.form['body']
+        title_error= ""
+        body_error = ""
+
+        if len(blog_title) < 1:
+            title_error = "Invalid body"
+
+        if not title_error and not body_error:
+            new_blog = Blog(blog_title, blog_body)
+            db.session.add(new_blog)
+            db.session.commit()
+            query_param_url = "/blog?id=" + str(new_blog.id)
+            return redirect(query_param_url)
+
+        else:
+            return render_template('newpost.html', title="Add Blog Entry", title_error=title_error, body_error=body_error)
+            
+
+
+@app.route('/blog', methods=['POST', 'GET'])
+def index():
+    if request.args:
+        blog_id = request.args("id")
+        blog = Blog.query.get(blog_id))
+
+
+    
+    
+if __name__== '__main__':
+    app.run()
