7d561e29e00030b6c1b74673a770720c169390e0
timcliffe <timothy.h.cliffe@gmail.com>
2017-10-09 19:31:33 -0500

    stuff

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from flask import Flask, request, redirect, request, render_template
-from flask_sqlalchemy import flask_sqlalchemy
+from flask_sqlalchemy 
+import SQLAlchemy
 
 app = Flask(__name__)
-ap.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql.pymysql://build-a-blog:Angels1234@localhost:8889/build-a-blog"
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://build-a-blog:Angels1234@localhost:8889/build-a-blog"
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
@@ -20,12 +21,12 @@ class Blog(db.Model):
 
 
 
-@app.route('/newpost', methods=['POST'])
+@app.route('/newpost', methods=['GET', 'POST'])
 def add_blog():
     if request.method == 'GET':
-        return render_template('newpost.html', title="Add Blog Entry"
+        return render_template('newpost.html', title="Add Blog Entry")
 
-    if request.method = 'POST':
+    if request.method == 'POST':
         blog_title = request.form['title']
         blog_body = request.form['body']
         title_error= ""
@@ -43,14 +44,22 @@ def add_blog():
 
         else:
             return render_template('newpost.html', title="Add Blog Entry", title_error=title_error, body_error=body_error)
-            
+
 
 
 @app.route('/blog', methods=['POST', 'GET'])
 def index():
     if request.args:
         blog_id = request.args("id")
-        blog = Blog.query.get(blog_id))
+        blog = Blog.query.get(blog_id)
+
+        return render_template('blogentry.html', blog=blog)
+
+    else:
+        blogs = Blog.query.all()
+
+        return render_template('blog.html', title="Build a Blog", blogs=blogs)
+
 
 
     
