29d3c3aab1f91e5f492f5561dac124a6cf0d4fe8
timcliffe <timothy.h.cliffe@gmail.com>
2017-10-16 18:12:00 -0500

    .

--- main.py
+++ main.py
@@ -1,26 +1,53 @@
-from flask import Flask, request, redirect, request, render_template
+from flask import Flask, request, redirect, request, render_template, session, flash
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://build-a-blog:Angels1234@localhost:8889/build-a-blog"
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://blogz:Angels1234@localhost:34000/blogz"
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.string(120), unique=True)
+    password = db.Column(db.String(120))
+
+    def __init__(self, email, password):
+        self.email = bloggers
+        self.password = password
+
+
 
 class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     body = db.Column(db.String(1000))
+    blogger_id = db.Column(db.Integer, db.ForeignKey('blogger.id'))
 
-    def __init__(self, title, body):
+    def __init__(self, title, body, blogger):
+        self.blogger = blogger
         self.title = title
         self.body = body
 
+@app.route('/blog')
+def blog():
+    user = request.args.get("blogger")
+    id = request.args.get("id")
+    blogs = Blog.query.get(id)
+    return render_template('blog.html', blogs=blogs)
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/signup')
+def register():
+    return render_template('signup.html')
 
 
 
-
-@app.route('/newpost', methods=['GET', 'POST'])
+@app.route('/blog/newpost', methods=['GET', 'POST'])
 def add_blog():
     if request.method == 'GET':
         return render_template('newpost.html', title="Add Blog Entry")
@@ -46,18 +73,12 @@ def add_blog():
 
 
 
+
+
 @app.route('/blog', methods=['POST', 'GET'])
 def index():
-    if request.args:
-        blog_id = request.args.get("id")
-        blog = Blog.query.get(blog_id)
-
-        return render_template('blogpost.html', blog=blog)
-
-    else:
-        blogs = Blog.query.all()
-
-        return render_template('blog.html', title="Build a Blog", blogs=blogs)
+    bloggers = user.query.all()
+    return render_template('index.html', bloggers=bloggers)
 
 
 
